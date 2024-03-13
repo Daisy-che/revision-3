b02ed5ad76e81abac37b41105f52e406c40c2c56
Student <daisychepkiruih@gmail.com>
2024-03-13 14:39:09 +0300

    add file

---
+++ length.py
@@ -0,0 +1,51 @@
+//use string formatting to create a message that includes variable for a perons name and age.print the formated message
+name="Daisy"
+age=25
+message= f"Hello ,my name is {name} and i am {age}yrs old"
+print(message)
+//Given the string "python retrieve and print the first and last  character using indexing."
+string="python"
+character1 = string[0]
+character2 = string[5]
+print(character1)
+print(character2)
+//Determine the Length of the String"Hello ,World!" in python
+S=" Hello World"
+len(S)
+//Given the string `"Programming"`, use slicing to extract the substring "gram."
+string="programming"
+substring = string[3:7]
+print(substring)
+//Slice the string `"abcdefgh"` to create a new string that includes only the chara
+string="abcdefgh"
+newString=string[2:6]
+print(newString)
+//Use the `count()` method to find the number of occurrences of the letter "a" in the string `"banana"`
+string="banana"
+string.count("a")
+//Convert the string `"hello"` to uppercase using the `upper()` method.
+a="hello"
+a.upper()
+//Replace all occurrences of the word "apple" with "orange" in the string `"I have an apple and another apple
+b= "I Have an apple and another apple"
+c=b.replace("apple","orange")
+//Concatenate the strings "Hello" and "World" to create the sentence "Hello World" in Python
+d= "hello world"
+p= hello + " "+world
+//Given the string `"Python is fun"`, check if the string starts with "Python" using the `startswith()` method.
+x="python is fun"
+y= x.startwith()
+//Convert the number `42` to a string in Python.
+num='42'
+b=str(num)
+b
+//Convert the string `"3.14"` to a floating-point number.
+pi="3.14"
+c=float(pi)
+c
+//Find the index of the first occurrence of the letter "o" in the string `"Python"`.
+d="python"
+d[4]
+//Check if the substring "world" is present in the string `"Hello, World!"` using the `in` keyword.
+t="Hello world"
+"world" in "hello World"
